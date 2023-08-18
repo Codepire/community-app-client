ede5e5b219291fd92b9f2c328d76afcfde5980cf
Viral Rupani <viralrupani12017@gmail.com>
2023-08-18 15:40:02 +0530

    feat: add footer placeholder

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <hr />
+      <div className="Footer__div">
+        <h1>Footer</h1>
+      </div>
+    </footer>
+  );
+}
