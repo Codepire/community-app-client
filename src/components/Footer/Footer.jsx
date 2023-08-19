402911b14a2f5c4728cf19b5549c82518ef48e09
AadiXC0DE <aadityaz2077@gmail.com>
2023-08-19 16:23:04 +0530

    created navbar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,135 @@ export default function Footer() {
     <footer>
       <hr />
       <div className="Footer__div">
-        <h1>Footer</h1>
+        <div class="footer-grid">
+          <div class="footer-logo">
+            <img src="logo.png" alt="Logo"></img>
+          </div>
+          <div class="footer-resources">
+            <h4 className="footer-headings">Resources</h4>
+            <ul className="footer-list">
+              <li>
+                <a className="footer-link" href="#">
+                  Docs
+                </a>
+              </li>
+              <li>
+                <a className="footer-link" href="#">
+                  Learn
+                </a>
+              </li>
+              <li>
+                <a className="footer-link" href="#">
+                  Showcase
+                </a>
+              </li>
+              <li>
+                <a className="footer-link" href="#">
+                  Blog
+                </a>
+              </li>
+              <li>
+                <a className="footer-link" href="#">
+                  Analytics
+                </a>
+              </li>
+              <li>
+                <a className="footer-link" href="#">
+                  Previews
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="footer-more">
+            <h4 className="footer-headings">More</h4>
+            <ul className="footer-list">
+              <li>
+                <a className="footer-link" href="#">
+                  Commerce
+                </a>
+              </li>
+              <li>
+                <a className="footer-link" href="#">
+                  Contact Sales
+                </a>
+              </li>
+              <li>
+                <a className="footer-link" href="#">
+                  GitHub
+                </a>
+              </li>
+              <li>
+                <a className="footer-link" href="#">
+                  Releases
+                </a>
+              </li>
+              <li>
+                <a className="footer-link" href="#">
+                  Telemetry
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="footer-about">
+            <h4 className="footer-headings">About</h4>
+            <ul className="footer-list">
+              <li>
+                <a className="footer-link" href="#">
+                  App Name
+                </a>
+              </li>
+              <li>
+                <a className="footer-link" href="#">
+                  Open Source Software
+                </a>
+              </li>
+              <li>
+                <a className="footer-link" href="#">
+                  GitHub
+                </a>
+              </li>
+              <li>
+                <a className="footer-link" href="#">
+                  Twitter
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="footer-legal">
+            <h4 className="footer-headings">Legal</h4>
+            <ul className="footer-list">
+              <li>
+                <a className="footer-link" href="#">
+                  Privacy Policy
+                </a>
+              </li>
+              <li>
+                <a className="footer-link" href="#">
+                  Cookie Preferences
+                </a>
+              </li>
+            </ul>
+            <div class="newsletter">
+              <p class="newsletter-text">Subscribe to our newsletter:</p>
+              <input
+                type="email"
+                class="newsletter-input"
+                placeholder="Your email"
+              ></input>
+            </div>
+          </div>
+        </div>
+        <div class="footer-bottom">
+          <p>&copy; 2023 Your Company Name</p>
+          <div class="social-icons">
+            <a href="www.xyz.com">
+              <img src="twitter.png" alt="Twitter"></img>
+            </a>
+            <a href="www.xyz.com">
+              <img src="github.png" alt="GitHub"></img>
+            </a>
+          </div>
+        </div>
       </div>
     </footer>
   );
