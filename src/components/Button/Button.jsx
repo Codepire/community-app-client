ef6d968f13575fd131519b2e8d8d4342ed59ef69
Viral Rupani <viralrupani12017@gmail.com>
2023-09-01 13:23:20 +0530

    fix&feat: button size issue & add new colors.

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,11 @@ import "./Button.css";
 
 export default function Button(props) {
   return (
-    <button className={props.buttonClassName} type={props.buttonType}>
+    <button
+      className={props.buttonClassName}
+      type={props.buttonType}
+      style={props.styles}
+    >
       {props.buttonText}
     </button>
   );
