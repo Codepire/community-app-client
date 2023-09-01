eda2758cee6f7400e7b5aefcfb1e75c6a4c41ace
Viral Rupani <viralrupani12017@gmail.com>
2023-09-01 13:24:37 +0530

    feat: add login and register.

--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -1,7 +1,78 @@
 import "./auth.css";
-  
+
+import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
+import { useState } from "react";
+
+import axios from "axios";
+
 export default function Login() {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setCredentials((prevCredentials) => {
+      return { ...prevCredentials, [name]: value };
+    });
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      const response = await axios({
+        url: "/auth/login",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: JSON.stringify({
+          username: credentials.username,
+          password: credentials.password,
+        }),
+      });
+      localStorage.setItem("jwtToken", response.data.token);
+    } catch (err) {
+      console.log("err");
+    } finally {
+      setCredentials({
+        username: "",
+        password: "",
+      });
+    }
+  }
+
   return (
-    <h1>Login</h1>
-  )
+    <>
+      <div className="Auth">
+        <form onSubmit={handleSubmit}>
+          <h2>Sign in</h2>
+          <Input
+            type="text"
+            name="username"
+            handleChange={handleChange}
+            placeholder="Username"
+            value={credentials.username}
+          />
+          <Input
+            type="password"
+            name="password"
+            handleChange={handleChange}
+            placeholder="Password"
+            value={credentials.password}
+          />
+          <Button
+            buttonClassName="btn btn-large btn-primary-hover"
+            buttonText="Continue"
+            buttonType="submit"
+            styles={{ width: "300px", margin: "7px 0" }}
+          />
+        </form>
+      </div>
+    </>
+  );
 }
--- src/pages/Authentication/Register.jsx
+++ src/pages/Authentication/Register.jsx
@@ -1,5 +1,84 @@
-export default function Register () {
+import "./auth.css";
+
+import Input from "../../components/Input/Input";
+import Button from "../../components/Button/Button";
+import { useState } from "react";
+import axios from "axios";
+
+export default function Register() {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setCredentials((prevCredentials) => {
+      return { ...prevCredentials, [name]: value };
+    });
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      const response = await axios({
+        url: "/auth/register",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          username: credentials.username,
+          email: credentials.email,
+          password: credentials.password,
+        },
+      });
+      console.log(response);
+    } catch (err) {
+      console.log("err");
+    } finally {
+      setCredentials({
+        username: "",
+        email: "",
+        password: "",
+      });
+    }
+  }
+
   return (
-    <h1>Register</h1>
-  )
+    <div className="Auth">
+      <form onSubmit={handleSubmit}>
+        <h2>Create Account</h2>
+        <Input
+          type="text"
+          name="username"
+          placeholder="Username"
+          handleChange={handleChange}
+          value={credentials.username}
+        />
+        <Input
+          type="email"
+          name="email"
+          placeholder="Email"
+          handleChange={handleChange}
+          value={credentials.email}
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Password"
+          handleChange={handleChange}
+          value={credentials.password}
+        />
+        <Button
+          buttonText="Continue"
+          buttonType="submit"
+          buttonClassName="btn btn-large btn-primary-hover"
+          styles={{ width: "300px", margin: "7px 0" }}
+        />
+      </form>
+    </div>
+  );
 }
